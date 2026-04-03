74a9acf2857f3e56a3e8b44e3892d9aff17b8e9c
dieterolson <dieterolson@gmail.com>
2026-04-03 08:03:44 -0500

    chore: remove scratch test_repro files that fail ruff lint

--- test_repro8.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-# Try importing something from pinocchio screw kinematics
-from src.engines.physics_engines.pinocchio.python.pinocchio_screw_kinematics import ScrewKinematicsAnalyzer
-
-print("Success!")
