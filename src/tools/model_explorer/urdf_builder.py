09120223873d5f938a9c32b4d76315b98fa90a3d
dieterolson <dieterolson@gmail.com>
2026-04-13 02:50:51 -0700

    style: auto format and fix linting

--- src/tools/model_explorer/urdf_builder.py
+++ src/tools/model_explorer/urdf_builder.py
@@ -223,7 +223,9 @@ class URDFBuilder:
 
         # Pretty print the XML
         rough_string = ET.tostring(robot, encoding="unicode")
-        reparsed = minidom.parseString(rough_string)  # nosec B318 - parsing internally generated ET output
+        reparsed = minidom.parseString(
+            rough_string
+        )  # nosec B318 - parsing internally generated ET output
         return str(reparsed.toprettyxml(indent="  "))
 
     def _create_empty_urdf(self) -> str:
@@ -241,7 +243,9 @@ class URDFBuilder:
         ET.SubElement(geometry, "box", size="0.1 0.1 0.1")
 
         rough_string = ET.tostring(robot, encoding="unicode")
-        reparsed = minidom.parseString(rough_string)  # nosec B318 - parsing internally generated ET output
+        reparsed = minidom.parseString(
+            rough_string
+        )  # nosec B318 - parsing internally generated ET output
         return str(reparsed.toprettyxml(indent="  "))
 
     def _add_materials(self, robot: ET.Element) -> None:
